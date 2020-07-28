679690dfe67c821d284e3bae9f524db86475b34e
Ruan Scherer <ruan.vscherer@gmail.com>
2020-07-28 17:54:48 -0300

    Queue created with workers

--- app/sender.py
+++ app/sender.py
@@ -1,24 +1,34 @@
 import psycopg2
-from bottle import route, run, request
+import redis
+import json
+from bottle import Bottle, request
 
-DSN = 'dbname=email_sender user=postgres host=db password=postgres'
-SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
+        self.queue = redis.StrictRedis(host='queue', port=6379, db=0)
+        DSN = 'dbname=email_sender user=postgres host=db password=postgres'
+        self.conn = psycopg2.connect(DSN)
 
-def register_message(subject, message):
-    conn = psycopg2.connect(DSN)
-    cursor = conn.cursor()
-    cursor.execute(SQL, (subject, message))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    print('Success')
 
-@route('/', method='POST')
-def send():
-    subject = request.forms.get('subject')
-    message = request.forms.get('message')
-    register_message(subject, message)
-    return 'Message queued! Subject: {} Message: {}'.format(subject, message)
+    def register_message(self, subject, message):
+        SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+        cursor = self.conn.cursor()
+        cursor.execute(SQL, (subject, message))
+        self.conn.commit()
+        cursor.close()
+
+        msg = {'subject': subject, 'message': message}
+        self.queue.rpush('sender', json.dumps(msg))
+        print('Success')
+
+    def send(self):
+        subject = request.forms.get('subject')
+        message = request.forms.get('message')
+        self.register_message(subject, message)
+        return 'Message queued! Subject: {} Message: {}'.format(subject, message)
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
