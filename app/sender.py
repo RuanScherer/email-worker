e8a0939ee9db8598660b7bc1aa20606f55e4cf56
Ruan Scherer <ruan.vscherer@gmail.com>
2020-07-28 17:26:32 -0300

    Networks created and insert email in database

--- app/sender.py
+++ app/sender.py
@@ -1,10 +1,24 @@
+import psycopg2
 from bottle import route, run, request
+
+DSN = 'dbname=email_sender user=postgres host=db password=postgres'
+SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+
+def register_message(subject, message):
+    conn = psycopg2.connect(DSN)
+    cursor = conn.cursor()
+    cursor.execute(SQL, (subject, message))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print('Success')
 
 @route('/', method='POST')
 def send():
     subject = request.forms.get('subject')
     message = request.forms.get('message')
-    return 'Message queued!'
+    register_message(subject, message)
+    return 'Message queued! Subject: {} Message: {}'.format(subject, message)
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080, debug=True)
